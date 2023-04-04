3361c8322fac38370c622b0466401aef238d6c16
Ping9 <sim2626@naver.com>
2023-04-04 20:52:48 +0900

    refactor [] Spring IoC 컨테이너 적용

--- src/main/java/hsyou/app/MemberApp.java
+++ src/main/java/hsyou/app/MemberApp.java
@@ -4,12 +4,15 @@ import hsyou.config.AppConfig;
 import hsyou.user.Grade;
 import hsyou.user.Member;
 import hsyou.user.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
 
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
 
         Member member = new Member(1L, "hsyou", Grade.VIP);
 
